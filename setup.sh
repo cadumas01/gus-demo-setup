b66f23e7ca14102953cbd210f25db07bbecd8445
cadumas01 <cadumas01@gmail.com>
2022-07-05 09:54:51 -0500

    update

--- setup.sh
+++ setup.sh
@@ -6,9 +6,6 @@ sudo wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
 # Install at root
 sudo tar -xvzf go1.15.2.linux-amd64.tar.gz 
 
-echo 'Installing git...'
-apt-get install git
-
 echo 'Installing python...'
 sudo apt-get update
 sudo apt-get install python3.8
