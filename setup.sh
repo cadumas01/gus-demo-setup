86fc69ceb7054139d08106d57f0ca48384689250
cadumas01 <cadumas01@gmail.com>
2022-07-08 20:28:54 -0400

    working?

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@
 echo 'Installing wget'
 sudo apt install wget
 
-echo 'Installing git...'
-apt-get install git
+#echo 'Installing git...'
+# apt-get install git
 
 echo 'Installing golang...'
 sudo wget -c https://golang.org/dl/go1.18.3.linux-amd64.tar.gz
@@ -19,23 +19,20 @@ source ~/.bashrc
 go version
 
 # echo 'Installing python...'
-# sudo apt-get update
-# sudo apt-get install python3
+sudo apt-get update
+sudo apt-get install python3
 
 
 # echo 'mkdir ~/go/src'
-# cd
-# mkdir go
-# cd go
-# mkdir src
-# cd src
+cd
+mkdir go
+cd go
+mkdir src
+cd src
 
 
 echo 'Install gus-automation (gcp branch)...'
-# sudo git clone -b gcp https://github.com/zhouaea/gus-automation.git
+sudo git clone -b gcp https://github.com/zhouaea/gus-automation.git
 
 echo 'Installing epaxos-demo...'
-# sudo git clone https://github.com/cadumas01/epaxos-demo.git
-
-# echo 'Installing gryff-testing...'
-# git clone https://github.com/bc-computing/gryff-testing.git
+sudo git clone https://github.com/cadumas01/epaxos-demo.git
